b2fc28de34abc65727c25443c8f606add26aab08
janixcool <johndalekirk@gmail.com>
2025-12-04 18:07:09 -0500

    Create No8.java

---
+++ No8.java
@@ -0,0 +1,11 @@
+public class No8
+{
+    public static void main (String [] args){
+        int[] A = {1, 2, 3, 4};
+        int[] B = new int[A.length];
+
+        for (int x = 0; x < A.length; x++) {
+            B[x] = A[x];
+        }
+    }
+}
